ff3dafed15af5b1d0f772c064e38623b2eaa3d6a
Julie Anne Moore <jmoore5@unb.ca>
2021-11-13 19:56:26 -0400

    started testing with Twilio getting started tutorial

--- Gemfile
+++ Gemfile
@@ -1,7 +1,13 @@
 source 'https://rubygems.org'
 ruby '2.7.2'
+
 gem 'minitest'
 gem 'rake'
 gem 'rubocop'
 gem 'rubocop-minitest'
 gem 'rubocop-rake'
+gem 'dotenv'
+
+# twilio requirments
+gem 'sinatra'
+gem 'twilio-ruby'
---
+++ lib/today_i_learned/send_sms.rb
@@ -0,0 +1,15 @@
+require 'twilio-ruby'
+require 'dotenv/load'
+
+account_sid = ENV['TWILIO_ACCOUNT_SID']
+auth_token = ENV['TWILIO_AUTH_TOKEN']
+client = Twilio::REST::Client.new(account_sid, auth_token)
+
+from = ENV['TWILIO_NUMBER']
+to = ENV['MOBILE_NUMBER']
+
+client.messages.create(
+  from: from,
+  to: to,
+  body: "Hey friend!"
+)
